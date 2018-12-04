6546da6eb05fe7f0f924497aa680ebb724b69b63
Naishadh Parmar <naishadh_parmar@live.com>
2018-12-04 15:55:48 +0530

    Finished computeCost() and key() functions.

--- path_planning/src/compute_cost.cpp
+++ path_planning/src/compute_cost.cpp
@@ -1,3 +1,4 @@
+#include<ros/ros.h>
 #include<bits/stdc++.h>
 #include<iostream>
 #include<math.h>
@@ -11,56 +12,77 @@ typedef struct node{
 }node;
 
 vector<vector<node> > vec;
-vector <node> g;
+vector<vector<double> > rhs;
+vector<vector<double> > g;
+node* start;
 
 double min(double a,double b){
-    if(a<=b)
-	return a;
-    else
-	return b;
+    if(a<=b) return a;
+    else return b;
 }
 
-//Function g(s) to be written that represents the path cost from s to the goal.
+node* determineAdjacent(node* s, node* s1, node* s2) {
+	node* adj;
+	if(s1.x == s2.x) {
+		if(s1.y > s2.y) adj = vec[s1.x][s1.y+1];
+		else adj = vec[s1.x][s1.y-1]
+	}
+	else if(s1.y == s2.y) {
+		if(s1.x > s2.x) adj = vec[s1.x+1][s1.y]
+		else adj = vec[s1.x-1][s1.y]
+	}
+	else ROS_ERROR("Cannot determine adjacent node");
+}
 
-void compute_cost(node s,node sa,node sb){//Keep in mind that the total path cost is path length * cost associated with that path.
-    if(sa is a diagonal neighborof s){
-	s1 = sb; s2 = sa;
+void compute_cost(node* s,node* sa, node* sb){//Keep in mind that the total path cost is path length * cost associated with that path.
+	node* s1, s2;
+    if(abs(sa->x-s->x) == 1 && abs(sa->y-s->y) == 1) s1 = sb; s2 = sa;
+    else s1 = sa; s2 = sb;
+
+	c = s->cost;
+	node* adj = determineAdjacent(s, s1, s2);
+	b = min(s.cost, adj.cost);
+
+	double vs;
+    if (min(c, b) == 1000000.0){
+		vs = 1000000.0;
+    }
+    else if (g[s1->x][s1->y] <= g[s2->x][s2->y]){
+        vs = min(c,b) + g[s1->x][s1->y]; //Fig 5(ii) of paper
     }
     else{
-	s1 = sa; s2 = sb;
+		double f = g[s1->x][s1->y] - g[s2->x][s2->y];
+		if (f <= b){
+			if (c <= f){
+				vs = c*sqrt(2) + g[s2->x][s2->y]; //Directly s to s2
+			}
+			else{
+				double y = min(f/sqrt(c*c-f*f), 1);
+				vs = c*sqrt(1+y*y) + f*(1 − y) + g[s2->x][s2->y]; //Fig 5(iv) of paper
+			}
+		}
+		else{
+			if (c <= b){
+				vs = c*sqrt(2) + g[s2->x][s2->y]; //Directly s to s2
+			}
+			else{
+				double x = 1 − min(b/sqrt(c*c-b*b), 1);
+				vs = c*sqrt(1+(1-x)*(1-x)) + b*x + g[s2->x][s2->y]; //Fig 5(iii) of paper
+			}
+		}
     }
+	return vs;
+}
 
-    c is traversal cost of cell with corners s, s1, s2;
-    b is traversal cost of cell with corners s, s1 but not s2;
+pair<double, double> key(node* s) {
+	double h = sqrt((s->x - start->x)*(s->x - start->x) + (s->y - start->y)*(s->y - start->y))/2
+	return make_pair(min(g[s->x][s->y], rhs[s->x][s->y]) + h, min(g[s->x][s->y], rhs[s->x][s->y]));
+}
 
-    if (min(c, b) = 1000000){
-	vs = 1000000;
-    }
-    else if (g(s1) ≤ g(s2)){
-        vs = min(c,b) + g(s1); //Fig 5(ii) of paper
-    }
-    else{
-	f = g(s1) − g(s2);
-	if (f ≤ b){
-	    if (c ≤ f){
-		vs = c*sqrt(2) + g(s2); //Directly s to s2
-	    }
-	    else{
-		y = min(f/sqrt(c*c-f*f) , 1);
-		vs = c*sqrt(1+y*y) + f(1 − y) + g(s2); //Fig 5(iv) of paper
-	    }
-	}
-	else{
-	    if (c ≤ b){
-		vs = c*sqrt(2) + g(s2); //Directly s to s2
-	    }
-	    else{
-		x = 1 − min( b/sqrt(c*c-b*b) , 1);
-		vs = c*sqrt(1+(1-x)*(1-x)) + bx + g(s2); //Fig 5(iii) of paper
-	    }
-	}
-    }
-return vs;
+queue <node> open;
+
+void updateState() {
+	if(g[s->x][s->y] - rhs[s->x][s->y] < 0.00001)
 }
 
 int main(){
